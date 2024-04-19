95930dbc5a50343e3563b440573dc3d37b0709e1
Konf1ic <kenbui2809@gmail.com>
2024-04-19 10:20:15 +0700

    SumOnDiagonal

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TinhTongDuongCheoChinh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kích thước 
+            Console.Write("Enter row: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter col: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            // Kiểm tra kích thước 
+            if (rows != cols)
+            {
+                Console.WriteLine("Error: Matrix is not square!!!");
+                return;
+            }
+
+            // Khởi tạo 
+            int[,] matrix = new int[rows, cols];
+
+            // Giá trị 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("Enter element [{0}, {1}]: ", i, j);
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine("Sum of elements on the main diagonal: {0}", sum);
+        }
+    }
+}
